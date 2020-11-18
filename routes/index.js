8d76140a2f0d1d9709715d87e9cee4f2c2d9ac81
mario010203 <mariojose1490@hotmail.com>
2020-11-18 19:50:56 +0100

    almost finished part 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const { brackets, parentheses } = require("../lib/helpers");
+
 /* GET home page */
 router.get("/", (req, res, next) => {
-	res.render("index");
+	const input = "53+44";
+	let bracketsCheck = brackets(input);
+
+	if (bracketsCheck !== null) {
+		bracketsCheck = bracketsCheck[1];
+	}
+
+	const result =
+		bracketsCheck !== null ? parentheses(bracketsCheck) : parentheses(input);
+
+	res.status(200).json(result);
 });
 
 module.exports = router;
